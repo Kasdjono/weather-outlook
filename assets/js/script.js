f72f30ab505c17d70707fe2a889589b096564513
Nick_Kash <nicholas.kasdjono.777@my.csun.edu>
2021-08-28 13:33:27 -0700

    inserted new api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 
 /* ------ city name input ------*/
 var cityInput = $('.cityInput');
-
+console.log(cityInput);
+cityInput = cityInput.val();
+console.log(cityInput);
 /* ------ saved city names ------*/
 var savedCityInput = $('.citySaved');
 
@@ -15,11 +17,29 @@ var submitButton = $('.submitButton')
 
 
 /*** ------ function for submit button ------ ***/
-
-/* ------ saving searches in local storage ------ */
 submitButton.on('click', function() {
-    cityInput = cityInput.val();
-    savedCityInput.push(cityInput);
-    localStorage.setItem("city",JSON.stringify(savedCityInput));
+
+    /* ------ saving searches in local storage ------ */
+    // cityInput = cityInput.val();
+    // savedCityInput.push(cityInput);
+    // localStorage.setItem("city",JSON.stringify(savedCityInput));
+
+    var apiKey = e6e814678fbf6b1aadbbcf2851cb710c;
+    console.log(apiKey);
+
+    var openWeatherUrl = 'api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=' + apiKey
+    console.log(cityInput);
+    console.log(openWeatherUrl);
+
+    fetch(openWeatherUrl)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (data) {
+
+        })
+
+
+
 
 })
