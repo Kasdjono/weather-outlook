7f7ebe5f76d026c4ab414999358bbb978b37a51f
Nick_Kash <nicholas.kasdjono.777@my.csun.edu>
2021-08-29 20:14:23 -0700

    found weather icon and finished README.md file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 /* ------ saved city names ------*/
-var savedCityInput = $('.citySaved');
+// var searchByCityEl = $('.history');
+// var searchHistoryEl = $('.')
 
 /* ------ search button ------ */
 var submitButton = $('.submitButton');
 
+/*** ------ Weather Information Section ------ */
 var latData;
 var lonData;
 var cityInput;
 
+var currCity = $('.currCity');
 var dispDay0 = $('.dispDay0');
 var dispDay1 = $('.dispDay1');
 var dispDay2 = $('.dispDay2');
@@ -17,11 +20,6 @@ var dispDay5 = $('.dispDay5');
 
 /*** ------ function for submit button ------ ***/
 submitButton.on('click', function () {
-
-    /* ------ saving searches in local storage ------ */
-    // cityInput = cityInput.val();
-    // savedCityInput.push(cityInput);
-    // localStorage.setItem("city",JSON.stringify(savedCityInput));
 
     /* ------ city name input ------*/
     var cityInput = $('.cityInput');
@@ -50,38 +48,82 @@ submitButton.on('click', function () {
             lonData = data.city.coord.lon;
             console.log(latData);
             console.log(lonData);
-            cityNameDisp = data.city.name;  // For displaying city properly regardless of user input
+
+            /* ------ current date/time display for current forecast ------ */
+            var liveTime
+            var currTime = moment().format(", dddd, MMMM Do YYYY");
+            liveTime = currTime;
+
+            /* ------ For displaying city properly and date/time regardless of user input ------ */
+            cityNameDisp = data.city.name;  
             console.log(cityNameDisp);
+            currCity.append(cityNameDisp + liveTime);
 
             /* ------ URL for current/5 day forecast fetch ------ */
-            var curropenWeatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latData + '&lon=' + lonData + '&exclude=minutely,hourly&appid=' + apiKey + "&units=imperial"
-            console.log(curropenWeatherUrl);
+            var currOpenWeatherUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latData + '&lon=' + lonData + '&exclude=minutely,hourly&appid=' + apiKey + "&units=imperial"
+            console.log(currOpenWeatherUrl);
 
             /* ------ fetch for current/5 day forecast ------ */
-            fetch(curropenWeatherUrl)
+            fetch(currOpenWeatherUrl)
                 .then(function (response) {
                     return response.json()
                 })
                 .then(function (data) {
                     console.log(data);
 
-                    /* ------ collect data for current/5 day forecast ------ */
-                    for (i=0; i < 6; i++) {
+                    /* ------ display data for current forecast ------ */
+                    var dayData = {
+                        tempData:"",
+                        humidityData:"",
+                        windSpeedData:"",
+                        uvIndexData:""
+                    };
+
+                    dayData.iconData = data.current.weather[0].icon;
+                    console.log(dayData.iconData);
+                    dayData.tempData = data.current.temp;
+                    dayData.humidityData = data.current.humidity;
+                    dayData.windSpeedData = data.current.wind_speed;
+                    dayData.uvIndexData = data.current.uvi;
+
+                    // var tempDataEl = $('<p>' + dayData.iconData + '<p>');
+                    // dispDay0.append(tempDataEl);
+
+                    var tempDataEl = $('<p>' + "Temp:  " + dayData.tempData + " F" + '<p>');
+                    dispDay0.append(tempDataEl);
+
+                    var windSpeedDataEl = $('<p>' + "Wind:  " + dayData.windSpeedData + " MPH" + '<p>');
+                    dispDay0.append(windSpeedDataEl);
+
+                    var humidityDataEl = $('<p>' + "Humidity:  " + dayData.humidityData + " %" + '<p>');
+                    dispDay0.append(humidityDataEl);
+
+                    var uvIndexDataEl = $('<p>' + "UV Index:  " + dayData.uvIndexData + '<p>');
+                    dispDay0.append(uvIndexDataEl);
+
+                    /* ------ display data for 5 day forecast ------ */
+                    for (i=0; i < 5; i++) {
+
+                        var dispDayArray = [dispDay1, dispDay2, dispDay3, dispDay4, dispDay5];
 
                         var dayData = {
+                            iconData:"",
                             tempData:"",
                             humidityData:"",
                             windSpeedData:"",
                             uvIndexData:""
                         };
                     
+                        dayData.iconData = data.daily[0].weather[0].icon;
+                        console.log(dayData.iconData)
+
                         dayData.tempData = data.daily[i].temp.day;
                         dayData.humidityData = data.daily[i].humidity;
                         dayData.windSpeedData = data.daily[i].wind_speed;
                         dayData.uvIndexData = data.daily[i].uvi;
-                        console.log(dayData);
                         
-                        var dispDayArray = [dispDay0, dispDay1, dispDay2, dispDay3, dispDay4, dispDay5];
+                        // var tempDataEl = $('<p>' + dayData.iconData + '<p>');
+                        // dispDayArray[i].append(tempDataEl);
 
                         var tempDataEl = $('<p>' + "Temp:  " + dayData.tempData + " F" + '<p>');
                         dispDayArray[i].append(tempDataEl);
@@ -94,49 +136,12 @@ submitButton.on('click', function () {
 
                         var uvIndexDataEl = $('<p>' + "UV Index:  " + dayData.uvIndexData + '<p>');
                         dispDayArray[i].append(uvIndexDataEl);
-
                                     
 
                     }
-
-                    
-                
-                
-
-                
-
-
+           
                 })
         })
     
 })
-
-
-                   
-
-                    // arrayDate[i] = $('<p>');
-                    // arrayDate[i].text(humidityData);
-
-                    // arrayDate[i] = $('<p>');
-                    // arrayDate[i].text(windSpeedData);
-
-                    // arrayDate[i] = $('<p>');
-                    // arrayDate[i].text(uvIndexData);
-
-
-
-                    // else {
-                       
-                    // }
-                     
- 
-                    // var dateData;
-                    // console.log(dateData);
-                    // var icon = data.daily.weather.icon;
-                    // console.log(icon);
-                    // var alert1 = data.alerts[0].event;
-                    // console.log(alert1);
-
-                    
-                    /* ------ passing forcast data to html file ------ */
 
